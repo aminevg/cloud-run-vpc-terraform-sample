9c0156bc835dde1092d74586edd3189f2547c32e
Amine Ilidrissi <38422328+aminevg@users.noreply.github.com>
2025-06-25 10:26:00 +0900

    Setup simple backend + frontend

--- packages/backend/index.ts
+++ packages/backend/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+import { randomUUIDv7 } from "bun"
+
+const server = Bun.serve({
+  port: process.env.BACKEND_PORT ?? "3000",
+  routes: {
+    "/random-uuid": (req) => {
+      return new Response(randomUUIDv7());
+    }
+  }
+})
+
+console.log(`Server is running on ${server.url}`);
